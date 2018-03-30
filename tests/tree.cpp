70270f75eba88640fe181d9412355da29ad48145
arsenygolubtsov <32494075+arsenygolubtsov@users.noreply.github.com>
2018-03-30 23:47:25 +0300

    Update tree.cpp

--- tests/tree.cpp
+++ tests/tree.cpp
@@ -17,8 +17,7 @@ TEST_CASE("insert and print tree")
 	std::string input3{"+3\n"};
 	std::string result{"----3\n"
 			   "--2\n"
-			   "----1\n"
-	};
+			   "----1\n"};
 	
 	std::istringstream istream1{ input1 };
 	std::istringstream istream2{ input2 };
@@ -30,3 +29,19 @@ TEST_CASE("insert and print tree")
 	tree.print(ostream, tree.root());
 	REQUIRE( ostream.str() == result );
 } 
+
+TEST_CASE("finding tree")
+{
+	tree_t tree;
+	std::string input1{"+2\n"};
+	std::string input2{"+1\n"};
+	std::string input3{"+3\n"};
+	std::istringstream istream1{ input1 };
+	std::istringstream istream2{ input2 };
+	std::istringstream istream3{ input3 };
+	REQUIRE(read(tree, istream1));
+	REQUIRE(read(tree, istream2));
+	REQUIRE(read(tree, istream3));
+	REQUIRE(tree.find(1) == true );
+	REQUIRE(tree.find(4) == false );
+}
