28fa7336fe5525fe5da93615bf8a6d66dbfb1b14
arsenygolubtsov <32494075+arsenygolubtsov@users.noreply.github.com>
2018-03-30 22:48:06 +0300

    Update tree.hpp

--- include/tree.hpp
+++ include/tree.hpp
@@ -1,27 +1,24 @@
 #include <iostream>
+#include <sstream>
+#include <string>
 
-class vector_t
+class tree_t
 {
 private:
-	int * elements_;
-	std::size_t size_;
-	std::size_t capacity_;
+    struct node_t {
+        node_t * left;
+        node_t * right;
+        int value;
+    };
+private:
+    node_t * root_;
 public:
-	vector_t();
-	vector_t(vector_t const & other);
-	vector_t & operator =(vector_t const & other);
-	~vector_t();
-
-	std::size_t size() const;
-	std::size_t capacity() const;
-
-	void push_back(int value);
-	void pop_back();
-
-	int & operator [](std::size_t index);
-	int operator [](std::size_t index) const;
-
-	bool operator ==(vector_t const & other) const;
+	tree_t() {
+        root_ = nullptr;
+    }
+	
+    void insert(int value);
+    bool find(int value) const;
+    void print(std::ostream & stream) const;
 };
 
-bool operator !=(vector_t const & lhs, vector_t const & rhs);
